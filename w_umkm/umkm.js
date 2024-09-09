46874070b598e473253b823cff3b0897da9da8a9
Yuana Bahtiar <77980123+Yuana-Bachtiar@users.noreply.github.com>
2024-09-09 09:55:34 +0700

    Add files via upload

---
+++ w_umkm/umkm.js
@@ -0,0 +1,16 @@
+// Toggle Class Active
+// const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika Hamburger Menu Di Klik
+// document.querySelector('#hamburger-menu').onlick = () => {
+//     navbarNav.classList.toggle('active');
+// };
+
+// Klik Diluar Sidebar Untuk Menghilangkan Navbar
+// const hamburger = document.querySelector('#hamburger-menu');
+
+// document.addEventListener('click', function(e){
+//     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+//         navbarNav.classList.remove('active');
+//     }
+// });
